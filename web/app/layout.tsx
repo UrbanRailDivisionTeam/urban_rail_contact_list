a2363462b064a35f80a3ed81825078450e3036f6
cheakf <sunmoming#gmail.com>
2026-05-20 16:03:02 +0800

    fix

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,8 +1,13 @@
+import type { Metadata } from "next"
 import { Geist, Geist_Mono, Inter } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils"
+
+export const metadata: Metadata = {
+    title: "城轨制造中心电话号码查询",
+}
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
 
